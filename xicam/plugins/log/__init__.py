89b2121d4587316767fc003bfa8a508d92359ad0
Ron Pandolfi <ronpandolfi@gmail.com>
2017-12-04 17:42:08 -0800

    Create stub for SAXS plugin

---
+++ xicam/plugins/log/__init__.py
@@ -0,0 +1,22 @@
+import numpy as np
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
+
+from xicam.core import msg
+
+from xicam.plugins import GUIPlugin, GUILayout
+
+
+
+class SAXSPlugin(GUIPlugin):
+    name = 'SAXS'
+    sigLog = Signal(int, str, str, np.ndarray)
+
+    def __init__(self):
+        self.stages = {'Calibrate': GUILayout(QLabel('Calibrate')),
+                       'Mask': GUILayout(QLabel('Mask')),
+                       'Reduce': GUILayout(QLabel('Reduce')),
+                       'Compare': GUILayout(QLabel('Compare'))
+                       }
+        super(SAXSPlugin, self).__init__()
