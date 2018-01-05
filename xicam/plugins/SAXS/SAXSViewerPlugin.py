1719d5c4dde60367297aca8964d8b0f265dbd2fc
Ron Pandolfi <ronpandolfi@gmail.com>
2018-01-05 10:53:15 -0800

    Add SAXSViewerPlugin (WIP)

--- xicam/plugins/SAXS/SAXSViewerPlugin.py
+++ xicam/plugins/SAXS/SAXSViewerPlugin.py
@@ -1,17 +1,73 @@
 from xicam.plugins import QWidgetPlugin
-from pyqtgraph import ImageView
-import lazyarray
+from pyqtgraph import ImageView, PlotItem
+from xicam.core.data import NonDBHeader
+from qtpy.QtWidgets import *
+from qtpy.QtCore import *
+import numpy as np
 
+class SAXSViewerPlugin(ImageView, QWidgetPlugin):
+    def __init__(self, *args, **kwargs):
 
-class SAXSViewerPlugin(QWidgetPlugin):
-    class widget(ImageView):
-        def __init__(self,*args,**kwargs):
-            super(SAXSViewerPlugin.widget, self).__init__(*args,**kwargs)
-            self.document = None
+        # Add q axes
+        self.axesItem = PlotItem()
+        self.axesItem.setLabel('bottom', u'q (Å⁻¹)')  # , units='s')
+        self.axesItem.setLabel('left', u'q (Å⁻¹)')
+        self.axesItem.axes['left']['item'].setZValue(10)
+        self.axesItem.axes['top']['item'].setZValue(10)
+        if 'view' not in kwargs: kwargs['view'] = self.axesItem
 
-        def setDocument(self, document, *args, **kwargs):
-            # make lazy array from document
+        super(SAXSViewerPlugin, self).__init__()
+        ImageView.__init__(self, *args, **kwargs)
+        self.axesItem.invertY(False)
 
+        # Setup axes reset button
+        self.resetAxesBtn = QPushButton('Reset Axes')
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.resetAxesBtn.sizePolicy().hasHeightForWidth())
+        self.resetAxesBtn.setSizePolicy(sizePolicy)
+        self.resetAxesBtn.setObjectName("resetBtn")
+        self.ui.gridLayout.addWidget(self.resetAxesBtn, 2, 1, 1, 1)
+        self.resetAxesBtn.clicked.connect(self.autoRange)
 
+        # Setup LUT reset button
+        self.resetLUTBtn = QPushButton('Reset LUT')
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.resetLUTBtn.sizePolicy().hasHeightForWidth())
+        self.resetLUTBtn.setSizePolicy(sizePolicy)
+        self.resetLUTBtn.setObjectName("resetLUTBtn")
+        self.ui.gridLayout.addWidget(self.resetLUTBtn, 2, 2, 1, 1)
+        self.resetLUTBtn.clicked.connect(self.autoLevels)
 
-            super(SAXSViewerPlugin.widget, self).setImage(data,*args,**kwargs)
+        # Setup coordinates label
+        self.coordinatesLbl = QLabel('--COORDINATES WILL GO HERE--')
+        self.ui.gridLayout.addWidget(self.coordinatesLbl,2,0,1,1,alignment=Qt.AlignHCenter)
+
+        # Use Viridis by default
+        self.setPredefinedGradient('viridis')
+
+        # Set background style #TODO: Make this styleable globally
+        self.setStyleSheet('background-color:black;')
+
+        self.document = None
+
+    def quickMinMax(self, data):
+        """
+        Estimate the min/max values of *data* by subsampling. MODIFIED TO USE THE 99TH PERCENTILE instead of max.
+        """
+        while data.size > 1e6:
+            ax = np.argmax(data.shape)
+            sl = [slice(None)] * data.ndim
+            sl[ax] = slice(None, None, 2)
+            data = data[sl]
+        return np.nanmin(data), np.nanpercentile(data, 99)
+
+    def setDocument(self, document: NonDBHeader, *args, **kwargs):
+        # make lazy array from document
+        data = document.meta_array('image')
+
+        super(SAXSViewerPlugin, self).setImage(img=data,*args,**kwargs)
+
--- xicam/plugins/SAXS/__init__.py
+++ xicam/plugins/SAXS/__init__.py
@@ -4,20 +4,20 @@ from qtpy.QtGui import *
 from qtpy.QtWidgets import *
 
 from xicam.core import msg
+from xicam.core.data import load_header
 
-from xicam.plugins import GUIPlugin, GUILayout
-
-
+from xicam.plugins import GUIPlugin, GUILayout, manager as pluginmanager
 
 class SAXSPlugin(GUIPlugin):
     name = 'SAXS'
     sigLog = Signal(int, str, str, np.ndarray)
 
     def __init__(self):
-        self.stages = {'Calibrate': GUILayout(QLabel('Calibrate')),
+        self.stages = {'Calibrate': GUILayout(pluginmanager.getPluginByName('SAXSViewerPlugin','WidgetPlugin').plugin_object()),
                        'Mask': GUILayout(QLabel('Mask')),
                        'Reduce': GUILayout(QLabel('Reduce')),
                        'Compare': GUILayout(QLabel('Compare'))
                        }
         super(SAXSPlugin, self).__init__()
 
+        self.stages['Calibrate'].centerwidget.setDocument(load_header(['/home/rp/data/YL1031/YL1031__2m_00000.edf','/home/rp/data/YL1031/YL1031__2m_00001.edf','/home/rp/data/YL1031/YL1031__2m_00002.edf']))
