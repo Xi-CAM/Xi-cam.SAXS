3595d893d448324e3d6e43437a8a136852d5d9e5
Ron Pandolfi <ronpandolfi@gmail.com>
2018-01-08 16:49:42 -0800

    Add file tree open hooks

--- xicam/plugins/SAXS/__init__.py
+++ xicam/plugins/SAXS/__init__.py
@@ -4,7 +4,7 @@ from qtpy.QtGui import *
 from qtpy.QtWidgets import *
 
 from xicam.core import msg
-from xicam.core.data import load_header
+from xicam.core.data import load_header,NonDBHeader
 
 from xicam.plugins import GUIPlugin, GUILayout, manager as pluginmanager
 from xicam.gui.widgets.dataresourcebrowser import DataResourceBrowser
@@ -14,12 +14,13 @@ class SAXSPlugin(GUIPlugin):
     sigLog = Signal(int, str, str, np.ndarray)
 
     def __init__(self):
-        self.stages = {'Calibrate': GUILayout(pluginmanager.getPluginByName('SAXSViewerPlugin','WidgetPlugin').plugin_object(),
-                                              left=DataResourceBrowser()),
+        self.stages = {'Calibrate': GUILayout(pluginmanager.getPluginByName('SAXSViewerPlugin','WidgetPlugin').plugin_object()),
                        'Mask': GUILayout(QLabel('Mask')),
                        'Reduce': GUILayout(QLabel('Reduce')),
                        'Compare': GUILayout(QLabel('Compare'))
                        }
         super(SAXSPlugin, self).__init__()
 
-        self.stages['Calibrate'].centerwidget.setDocument(load_header(['/home/rp/data/YL1031/YL1031__2m_00000.edf','/home/rp/data/YL1031/YL1031__2m_00001.edf','/home/rp/data/YL1031/YL1031__2m_00002.edf']))
+
+    def appendHeader(self, doc:NonDBHeader, **kwargs):
+        self.stages['Calibrate'].centerwidget.setHeader(doc)
