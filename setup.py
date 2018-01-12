b53acc7324b64a62bd382d31b05d680b0e1a663f
Ron Pandolfi <ronpandolfi@gmail.com>
2018-01-11 19:34:09 -0800

    Add pyFAI dependency

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'qtpy'],
+    install_requires=['numpy', 'qtpy', 'pyFAI'],
 
     setup_requires=[],
 
