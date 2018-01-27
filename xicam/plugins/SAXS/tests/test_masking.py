4acec315aeeed6474b56029b6c47837a581b2480
Guillaume Freychet <gfreychet@lbl.gov>
2018-01-26 16:46:15 -0800

    Implement cuts test

--- xicam/plugins/SAXS/tests/test_masking.py
+++ xicam/plugins/SAXS/tests/test_masking.py
@@ -1,8 +1,9 @@
-def test_VerticalCut(data, qz):
-    from ..processing.verticalcuts import VerticalCutPlugin
+def test_VerticalCut():
+    from xicam.plugins.SAXS.processing.verticalcuts import VerticalCutPlugin
+    import numpy as np
     t1 = VerticalCutPlugin()
-    t1.data.value = data
-    t1.qz.value = qz
+    t1.data.value = np.zeros((20,20))
+    t1.qz.value = np.tile(np.linspace(0,1, 20), (1, 20))
     t1.qzminimum.value = 0.1
     t1.qzmaximum.value = 0.3
     t1.evaluate()
