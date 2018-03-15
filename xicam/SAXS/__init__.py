cd9a13a3e94776833e1bd4da9375450b0af93cd9
ronpandolfi <ronpandolfi@gmail.com>
2018-03-15 11:08:25 -0700

    Add masking features to SAXS

--- xicam/SAXS/__init__.py
+++ xicam/SAXS/__init__.py
@@ -12,13 +12,14 @@ from xicam.core.execution.workflow import Workflow
 
 from xicam.plugins import GUIPlugin, GUILayout, manager as pluginmanager
 from .calibration import CalibrationPanel
-from .masking import MaskingPanel
 from .widgets.SAXSMultiViewer import SAXSMultiViewerPlugin
 from .widgets.SAXSViewerPlugin import SAXSViewerPlugin
 from .widgets.SAXSToolbar import SAXSToolbar
 from .widgets.SAXSSpectra import SAXSSpectra
 from xicam.gui.widgets.linearworkfloweditor import WorkflowEditor
 from pyFAI import AzimuthalIntegrator, detectors, calibrant
+import pyqtgraph as pg
+from functools import partial
 
 from xicam.gui.widgets.tabview import TabView, TabViewSynchronizer
 
@@ -86,15 +87,88 @@ class SAXSPlugin(GUIPlugin):
         def setAI(result):
             self.calibrationsettings.setAI(result['ai'].value, device)
 
-        workflow.execute(None, data=data, ai=ai, calibrant=c, callback_slot=setAI)
+        workflow.execute(None, data=data, ai=ai, calibrant=c, callback_slot=setAI, threadkey='calibrate')
 
     def doMaskingWorkflow(self, workflow: Workflow):
-        data = self.calibrationtabview.currentWidget().header.meta_array('pilatus2M_image')[0]
-        ai = self.calibrationsettings.AI('pilatus2M')
-        ai.detector = detectors.Pilatus2M()
-        outputwidget = self.masktabview.currentWidget()
+        if not self.checkPolygonsSet(workflow):
+            data = self.calibrationtabview.currentWidget().header.meta_array('pilatus2M_image')[0]
+            ai = self.calibrationsettings.AI('pilatus2M')
+            ai.detector = detectors.Pilatus2M()
+            outputwidget = self.masktabview.currentWidget()
 
-        def showMask(result):
-            outputwidget.setMaskImage(result['mask'].value)
+            def showMask(result=None):
+                if result:
+                    outputwidget.setMaskImage(result['mask'].value)
+                else:
+                    outputwidget.setMaskImage(None)
 
-        workflow.execute(None, data=data, callback_slot=showMask)
+            workflow.execute(None, data=data, ai=ai, callback_slot=showMask, threadkey='masking')
+
+    def checkPolygonsSet(self, workflow: Workflow):
+        """
+        Check for any unset polygonmask processes
+
+        Parameters
+        ----------
+        workflow: Workflow
+
+        Returns
+        -------
+        bool
+            True if unset polygonmask process is found
+
+        """
+        pluginmaskclass = pluginmanager.getPluginByName('Polygon Mask', 'ProcessingPlugin')
+        for process in workflow.processes:
+            if isinstance(process, pluginmaskclass.plugin_object):
+                if process.polygon.value is None:
+                    self.startPolygonMasking(process)
+                    return True
+        return False
+
+    def startPolygonMasking(self, process):
+        self.setEnabledOuterWidgets(False)
+
+        # Start drawing mode
+        viewer = self.masktabview.currentWidget()  # type: SAXSViewerPlugin
+        viewer.imageItem.setDrawKernel(kernel=np.array([[0]]), mask=None, center=(0, 0), mode='add')
+        viewer.imageItem.drawMode = self.drawEvent
+        viewer.maskROI.clearPoints()
+
+        # Setup other signals
+        process.parameter.child('Finish Mask').sigActivated.connect(partial(self.finishMask, process))
+        process.parameter.child('Clear Selection').sigActivated.connect(self.clearMask)
+
+    def setEnabledOuterWidgets(self, enabled):
+        # Disable other widgets
+        mainwindow = self.masktabview.window()
+        for dockwidget in mainwindow.findChildren(QDockWidget):
+            dockwidget.setEnabled(enabled)
+        mainwindow.rightwidget.setEnabled(True)
+        self.maskeditor.workflowview.setEnabled(enabled)
+        self.masktabview.tabBar().setEnabled(enabled)
+        mainwindow.menuBar().setEnabled(enabled)
+        mainwindow.pluginmodewidget.setEnabled(enabled)
+
+    def clearMask(self):
+        viewer = self.masktabview.currentWidget()  # type: SAXSViewerPlugin
+        viewer.maskROI.clearPoints()
+
+    def finishMask(self, process, sender):
+        viewer = self.masktabview.currentWidget()  # type: SAXSViewerPlugin
+        process.polygon.value = np.array([list(handle['pos']) for handle in viewer.maskROI.handles])
+        self.setEnabledOuterWidgets(True)
+
+        # End drawing mode
+        viewer.imageItem.drawKernel = None
+        viewer.maskROI.clearPoints()
+        process.parameter.clearChildren()
+
+        # Redo workflow with polygon
+        self.doMaskingWorkflow(process._workflow)
+
+    def drawEvent(self, kernel, imgdata, mask, ss, ts, event):
+        viewer = self.masktabview.currentWidget()  # type: SAXSViewerPlugin
+        viewer.maskROI.addFreeHandle(viewer.view.vb.mapSceneToView(event.scenePos()))
+        if len(viewer.maskROI.handles) > 1:
+            viewer.maskROI.addSegment(viewer.maskROI.handles[-2]['item'], viewer.maskROI.handles[-1]['item'])
