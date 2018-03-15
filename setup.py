74ecc8174f6277bf952bfaa8ed6ab6c70c96d935
ronpandolfi <ronpandolfi@gmail.com>
2018-03-15 11:07:47 -0700

    add dependent packages

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'qtpy', 'pyFAI'],
+    install_requires=['numpy', 'qtpy', 'pyFAI', 'scipy', 'dill', ],
 
     setup_requires=[],
 
