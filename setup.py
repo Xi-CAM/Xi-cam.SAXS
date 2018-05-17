de8294994e36cd2cd7ef92dfd8a43d3ca29fb7ca
ronpandolfi <ronpandolfi@gmail.com>
2018-05-17 14:53:13 -0700

    remove sasmodels as dependency

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'qtpy', 'pyFAI', 'scipy', 'dill', 'astroscrappy', 'sasmodels'],
+    install_requires=['numpy', 'qtpy', 'pyFAI', 'scipy', 'dill', 'astroscrappy'],
 
     setup_requires=[],
 
