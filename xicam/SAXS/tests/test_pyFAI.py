d24bc71d54ad4696322c3adf01032810ec9098e2
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-10-16 15:33:29 -0700

    Fix tests

--- xicam/SAXS/tests/test_pyFAI.py
+++ xicam/SAXS/tests/test_pyFAI.py
@@ -1,8 +1,8 @@
 # from xicam.SAXS.patches.pyFAI import *
 import dill
 import pyFAI
-from distributed.protocol.serialize import serialize as dumps, deserialize as loads
-
+# from distributed.protocol.serialize import serialize as dumps, deserialize as loads
+from pickle import dumps, loads
 
 def test_Detector_pickle():
     import cloudpickle
@@ -17,8 +17,8 @@ def test_Detector_pickle():
     # print(det.__getstate__())
 
     assert dumps(det)
-    assert loads(*dumps(det))
-    assert loads(*dumps(det)).shape == (1679, 1475)
+    assert loads(dumps(det))
+    assert loads(dumps(det)).shape == (1679, 1475)
 
 
 def test_AzimuthalIntegrator_pickle():
@@ -31,6 +31,13 @@ def test_AzimuthalIntegrator_pickle():
     ai.set_wavelength(.1)
     spectra = ai.integrate1d(np.ones(det.shape), 1000)  # force lut generation
     dump = dumps(ai)
-    newai = loads(*dump)
+    newai = loads(dump)
     assert np.array_equal(newai.integrate1d(np.ones(det.shape), 1000), spectra)
     assert newai.detector.shape == (1679, 1475)
+
+
+def test_Calibrant():
+    from pyFAI import calibrant
+    calibrant = calibrant.CalibrantFactory()('AgBh')
+    assert dumps(calibrant)
+    assert loads(dumps(calibrant))
