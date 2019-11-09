55c1b82af8699c9892fd78b50a44f9cde39959f1
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-11-08 16:15:55 -0800

    Add FastCCD detector module

---
+++ xicam/SAXS/detectors/__init__.py
@@ -0,0 +1,24 @@
+from pyFAI import detectors
+import numpy as np
+from collections import OrderedDict
+
+
+class FastCCD(detectors.Detector):
+    aliases = ['Fast CCD']
+    MAX_SHAPE = (1000, 960)
+
+    def calc_mask(self):
+        """
+        Returns a generic mask for FastCCD detectors...
+        """
+
+        mask = np.zeros((1000, 960))
+        # mask middle 19 rows
+        mask[500 - 19 / 2:500 + 19 / 2] = 1
+        # TODO: check that this mask is correct
+
+        return mask
+
+
+if __name__ == '__main__':
+    assert detectors.ALL_DETECTORS['fastccd']
