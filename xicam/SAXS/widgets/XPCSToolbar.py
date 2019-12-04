34eb8ac4f479a47713240e775255a175a79cafe7
Ian Humphrey <ihumphrey@lbl.gov>
2019-12-04 11:35:48 -0800

    Add XPCSToolbar blend for correlation toolbar

---
+++ xicam/SAXS/widgets/XPCSToolbar.py
@@ -0,0 +1,26 @@
+from qtpy.QtCore import Qt
+from .SAXSToolbar import SAXSToolbarBase, ROIs
+
+
+# TODO -- move these to more reusable area
+class Button(SAXSToolbarBase):
+    def __init__(self, *args, button_icon_path=None, button_text="", receiver=None, **kwargs):
+        super(Button, self).__init__(*args, **kwargs)
+        action = self.mkAction(button_icon_path, button_text, receiver, **kwargs)
+        self.addAction(action)
+        # if button_text:
+        #     self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+
+
+class ProcessButton(Button):
+    """Defines a button that can be used to start a process."""
+    def __init__(self, *args, button_receiver=None, **kwargs):
+        super(ProcessButton, self).__init__(*args,
+                                            button_icon_path="icons/run.png",
+                                            button_text="Process",
+                                            receiver=button_receiver,
+                                            **kwargs)
+
+
+class XPCSToolBar(ROIs, ProcessButton):
+    pass
