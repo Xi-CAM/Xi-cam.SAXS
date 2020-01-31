92874cd0e1d6a49e7b562975db3f0ae5e51179fb
Ronald Pandolfi <ronpandolfi@gmail.com>
2020-01-30 16:54:00 -0800

    Add Lambda device profile; update FastCCD with pixel size

--- xicam/SAXS/detectors/__init__.py
+++ xicam/SAXS/detectors/__init__.py
@@ -7,6 +7,9 @@ class FastCCD(detectors.Detector):
     aliases = ['Fast CCD']
     MAX_SHAPE = (1000, 960)
 
+    def __init__(self, pixel1=30.e-6, pixel2=30.e-6, max_shape=MAX_SHAPE):
+        super(FastCCD, self).__init__(pixel1=pixel1, pixel2=pixel2, max_shape=max_shape)
+
     def calc_mask(self):
         """
         Returns a generic mask for FastCCD detectors...
@@ -18,6 +21,14 @@ class FastCCD(detectors.Detector):
         # TODO: check that this mask is correct
 
         return mask
+
+
+class LAMBDA(detectors.Detector):
+    aliases = ['LAMBDA']
+    MAX_SHAPE = (1536, 512)
+
+    def __init__(self, pixel1=22.e-6, pixel2=22.e-6, max_shape=MAX_SHAPE):
+        super(LAMBDA, self).__init__(pixel1=pixel1, pixel2=pixel2, max_shape=max_shape)
 
 
 if __name__ == '__main__':
