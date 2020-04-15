3a747c64df4dfbbb6ff4bc7f7a5fd6b3ed5df1c8
Juliane Reinhardt <jreinhardt@lbl.gov>
2020-04-14 23:59:13 -0700

    arrayrotate.py,  correction.py, onetime.py transition to operationplugins

--- xicam/SAXS/processing/arrayrotate.py
+++ xicam/SAXS/processing/arrayrotate.py
@@ -1,12 +1,22 @@
-from xicam.plugins import ProcessingPlugin, Input, Output, InOut
+from xicam.plugins.operationplugin import OperationPlugin, describe_input, describe_output, \
+     output_names, categories
 import numpy as np
 
+@OperationPlugin
+@describe_input('data', 'Input array of two or more dimensions')
+@describe_input('number_of_rotation', 'Number of times the array is rotated by 90 degrees')
+@describe_input('axis_of_rotation', 'The array is rotated in the plane defined by the axes. Axes must be different.')
+@describe_output('data', 'Output array of two or more dimensions')
+@output_names('data')
+@categories('Transformations')
 
-class ArrayRotate(ProcessingPlugin):
-    data = InOut(description='Array of two or more dimensions.', type=np.ndarray)
-    k = Input(description='Number of times the array is rotated by 90 degrees.', type=int, default=1)
-    axes = Input(description='The array is rotated in the plane defined by the axes. Axes must be different.',
-                 type=np.ndarray, default=(0, 1))
 
-    def evaluate(self):
-        self.data.value = np.rot90(self.data.value, self.k.value, self.axes.value)
+def rotate_array(data: np.ndarray,
+                number_of_rotation: int = 1,
+                axis_of_rotation: np.ndarray = (0,1)) -> np.ndarray:
+    
+     data = np.rot90(data, number_of_rotation, axis_of_rotation)
+     return data
+
+
+
