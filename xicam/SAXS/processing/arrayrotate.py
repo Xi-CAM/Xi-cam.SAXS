1947b0285f3cb226fc8b3895c5e7e8eceb1c8848
Ronald Pandolfi <ronpandolfi@gmail.com>
2020-04-28 13:19:50 -0700

    Fix typo

--- xicam/SAXS/processing/arrayrotate.py
+++ xicam/SAXS/processing/arrayrotate.py
@@ -1,4 +1,4 @@
 from xicam.plugins.operationplugin import operation
 import numpy as np
 
-rotate_array = operation(np.rot90, name="Rotate Array", categores=(("General", "Mathematics"),))
+rotate_array = operation(np.rot90, name="Rotate Array", categories=(("General", "Mathematics"),))
