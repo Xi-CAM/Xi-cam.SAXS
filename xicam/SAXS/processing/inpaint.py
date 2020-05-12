6bef4b8e862190285e40dcc24160628f6394a4e1
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-05-12 11:50:53 -0700

    Wrap pyFAI reconstruct C function in inpaint

--- xicam/SAXS/processing/inpaint.py
+++ xicam/SAXS/processing/inpaint.py
@@ -2,5 +2,14 @@ from xicam.plugins.operationplugin import operation
 import numpy as np
 from pyFAI.ext.reconstruct import reconstruct
 
-inpaint = operation(reconstruct, name='Inpaint (pyFAI.ext.reconstruct)')
+
+# Wraps the C implemented 'reconstruct' function to provide inspect metadata
+def _reconstruct(ndarray_data: np.ndarray,
+                 ndarray_mask: np.ndarray = None,
+                 dummy: float = None,
+                 delta_dummy: float = None):
+    return reconstruct(ndarray_data, ndarray_mask, dummy, delta_dummy)
+
+
+inpaint = operation(_reconstruct, name='Inpaint (pyFAI.ext.reconstruct)')
 # TODO: add categories
