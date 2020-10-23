6838213e930e9a4abcc4e36acab6c17a63ed40e3
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-10-23 14:38:45 -0700

    fix indentation

--- xicam/SAXS/processing/correction.py
+++ xicam/SAXS/processing/correction.py
@@ -20,47 +20,47 @@ def correct_fastccd_image(images: np.ndarray,
                           darks: np.ndarray = None,
                           gains: tuple = (1, 2, 4, 8)) -> np.ndarray:
 
-        def correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
-            # 16-bit unsigned
-            # image: bits 0 - 12
-            # bad:   bit 13
-            # gain:  bits 14-15
-            # gain1: 0b11 (3)
-            # gain2: 0b10 (2)
-            # gain8: 0b00 (0)
-            intensity = np.bitwise_and(0x1FFF, array)
-            bad_flag = np.bitwise_and(0x1, np.right_shift(array, 13))
-            gain = np.bitwise_and(0x3, np.right_shift(array, 14))
-            # map the gain bit values to the gain map indices to get the actual gain values
-            # e.g. 3 -> index 2; 2 -> index 1; 1 -> index 0
-            gain_map = dict(zip((0, 1, 2, 3), gain_map))
-            gain = np.vectorize(gain_map.get)(gain)
-            # gain = np.vectorize(partial(lambda a, b: a[int((b + 1) / 2)], gain_map))(gain)
-            arr = flats * gain * intensity
-            arr = np.where(arr < bkg, bkg, arr)
-            return np.array((1 - bad_flag) * (arr - bkg), dtype=np.uint16)
+    def correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
+        # 16-bit unsigned
+        # image: bits 0 - 12
+        # bad:   bit 13
+        # gain:  bits 14-15
+        # gain1: 0b11 (3)
+        # gain2: 0b10 (2)
+        # gain8: 0b00 (0)
+        intensity = np.bitwise_and(0x1FFF, array)
+        bad_flag = np.bitwise_and(0x1, np.right_shift(array, 13))
+        gain = np.bitwise_and(0x3, np.right_shift(array, 14))
+        # map the gain bit values to the gain map indices to get the actual gain values
+        # e.g. 3 -> index 2; 2 -> index 1; 1 -> index 0
+        gain_map = dict(zip((0, 1, 2, 3), gain_map))
+        gain = np.vectorize(gain_map.get)(gain)
+        # gain = np.vectorize(partial(lambda a, b: a[int((b + 1) / 2)], gain_map))(gain)
+        arr = flats * gain * intensity
+        arr = np.where(arr < bkg, bkg, arr)
+        return np.array((1 - bad_flag) * (arr - bkg), dtype=np.uint16)
 
-        # TODO: is this pulling from the correct dark? we need of mapping of gain index to dark array?
+    # TODO: is this pulling from the correct dark? we need of mapping of gain index to dark array?
 
-        if images.value.ndim not in [2, 3]:
-            raise ValueError(f"\"images\" expects a 2- or 3-dimensional image array; shape = \"{images.value.shape}\"")
+    if images.value.ndim not in [2, 3]:
+        raise ValueError(f"\"images\" expects a 2- or 3-dimensional image array; shape = \"{images.value.shape}\"")
 
-        flats = flats.value
-        if flats is None:
-            flats = 1
-        elif flats.ndim != 2:
-            raise ValueError(f"\"flats\" should be 2-dimensional; shape = \"{flats.value.shape}\"")
+    flats = flats.value
+    if flats is None:
+        flats = 1
+    elif flats.ndim != 2:
+        raise ValueError(f"\"flats\" should be 2-dimensional; shape = \"{flats.value.shape}\"")
 
-        darks = darks.value
-        if darks is None:
-            darks = 0
-        elif darks.ndim != 3:
-            raise ValueError(f"\"darks\" should be 3-dimensional; shape = \"{darks.value.shape}\"")
-        else:
-            darks = np.sum(darks, axis=0) / darks.shape[0]
+    darks = darks.value
+    if darks is None:
+        darks = 0
+    elif darks.ndim != 3:
+        raise ValueError(f"\"darks\" should be 3-dimensional; shape = \"{darks.value.shape}\"")
+    else:
+        darks = np.sum(darks, axis=0) / darks.shape[0]
 
-        corrected_images.value = correct(np.asarray(images.value, dtype=np.uint16),
-                                         np.asarray(flats, dtype=np.float32),
-                                         np.asarray(darks, dtype=np.float32),
-                                         gains)
+    corrected_images.value = correct(np.asarray(images.value, dtype=np.uint16),
+                                     np.asarray(flats, dtype=np.float32),
+                                     np.asarray(darks, dtype=np.float32),
+                                     gains)
 
