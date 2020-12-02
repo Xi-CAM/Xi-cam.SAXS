ce93f0e5700dd8d572f3241ce3f8c63e31d5a721
ronpandolfi <ronpandolfi@gmail.com>
2020-12-02 11:46:10 -0800

    Make xpcs projector an optional import; give guiplugin a name; use relative import

--- xicam/SAXS/SAXSGUIPlugin.py
+++ xicam/SAXS/SAXSGUIPlugin.py
@@ -1,15 +1,30 @@
 from databroker.core import BlueskyRun
 
 from xicam.core.workspace import Ensemble
-from xicam.XPCS.projectors.nexus import project_nxXPCS
+from .mixins import CorrelationGUIPlugin
 
-from xicam.SAXS.mixins import CorrelationGUIPlugin, TemporarySAXSGUIPlugin
+projectors = []
+
+try:
+    from xicam.XPCS.projectors.nexus import project_nxXPCS
+except ImportError:
+    pass
+else:
+    projectors.append(project_nxXPCS)
 
 
-class SAXSPlugin(TemporarySAXSGUIPlugin, CorrelationGUIPlugin):
+def project_all(run_catalog:BlueskyRun):
+    for projector in projectors:
+        intents = projector(run_catalog)
+        if intents:
+            return intents
+
+
+class SAXSPlugin(CorrelationGUIPlugin):
+    name = 'SAXS'
 
     def appendCatalog(self, catalog: BlueskyRun, **kwargs):
         # catalog.metadata.update(self.schema())
         ensemble = Ensemble()
         ensemble.append_catalog(catalog)
-        self.ensembleModel.add_ensemble(ensemble, project_nxXPCS)
+        self.ensembleModel.add_ensemble(ensemble, project_all)
