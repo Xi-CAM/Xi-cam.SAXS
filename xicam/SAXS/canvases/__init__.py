af9d1fe4befffe850039aff0bf38f7ff09471af5
Ian Humphrey <ian.r.humphrey@gmail.com>
2020-12-16 16:19:25 -0800

    Add toolbar mixin to saxs image blend

--- xicam/SAXS/canvases/__init__.py
+++ xicam/SAXS/canvases/__init__.py
@@ -1,13 +1,23 @@
+from xicam.SAXS.widgets.SAXSToolbar import SAXSToolbarBase, ROIs
+from xicam.plugins import live_plugin
 from xicam.gui.canvases import ImageIntentCanvas
 from xicam.gui.widgets.imageviewmixins import LogScaleIntensity, ImageViewHistogramOverflowFix, \
-    QCoordinates, Crosshair, BetterButtons, CenterMarker
+    QCoordinates, Crosshair, BetterButtons, CenterMarker, ToolbarLayout
 
 
+@live_plugin("ImageMixinPlugin")
+class SAXSToolbarMixin(ToolbarLayout):
+    def __init__(self, *args, **kwargs):
+        toolbar = ROIs()
+        super(SAXSToolbarMixin, self).__init__(*args, toolbar=toolbar, **kwargs)
+
+
+@live_plugin("ImageMixinPlugin")
 class SAXSImageIntentBlend(LogScaleIntensity, CenterMarker, BetterButtons, Crosshair, QCoordinates,
-                           ImageViewHistogramOverflowFix):
+                           ImageViewHistogramOverflowFix, SAXSToolbarMixin):
     ...
 
 
-class SAXSImageIntentCanvas(ImageIntentCanvas, SAXSImageIntentBlend):
+class SAXSImageIntentCanvas(ImageIntentCanvas):
     def __init__(self, *args, **kwargs):
         super(SAXSImageIntentCanvas, self).__init__(*args, **kwargs)
