426f26b542ced29ee7c8e476deae722f6678b894
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-02-09 16:46:03 -0800

    add nxsas and nxcansas projectors to saxs project_all

--- xicam/SAXS/SAXSGUIPlugin.py
+++ xicam/SAXS/SAXSGUIPlugin.py
@@ -1,10 +1,14 @@
 from PyQt5.QtWidgets import QLabel
 from databroker.core import BlueskyRun
+from xicam.core import msg
 
 from xicam.core.workspace import Ensemble
-from .mixins import CorrelationGUIPlugin
+from xicam.core.data import ProjectionException
+from xicam.SAXS.mixins import CorrelationGUIPlugin
+from xicam.SAXS.projectors.nxcansas import project_nxcanSAS
+from xicam.SAXS.projectors.edf import project_NXsas
 
-projectors = []
+projectors = [project_NXsas, project_nxcanSAS]
 
 try:
     from xicam.XPCS.projectors.nexus import project_nxXPCS
@@ -14,11 +18,14 @@ else:
     projectors.append(project_nxXPCS)
 
 
-def project_all(run_catalog:BlueskyRun):
+def project_all(run_catalog: BlueskyRun):
     for projector in projectors:
-        intents = projector(run_catalog)
-        if intents:
-            return intents
+        try:
+            intents = projector(run_catalog)
+            if intents:
+                return intents
+        except ProjectionException:
+            pass
 
 
 class SAXSPlugin(CorrelationGUIPlugin):
