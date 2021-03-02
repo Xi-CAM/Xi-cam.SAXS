746dd436d617142c63679f1621d4812fc6fa9ac8
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-03-02 11:24:08 -0800

    add synthetic op to saxs operations

---
+++ xicam/SAXS/operations/synthetic.py
@@ -0,0 +1,9 @@
+import numpy as np
+from xicam.plugins.operationplugin import operation, output_names
+from xicam.plugins import live_plugin
+
+@live_plugin("OperationPlugin")
+@operation
+@output_names("images")
+def synthetic_image_series(images: np.ndarray, n: int = 10) -> np.ndarray:
+    return np.array([np.random.poisson(images) for i in range(n)])
