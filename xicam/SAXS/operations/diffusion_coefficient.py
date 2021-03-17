664c88fe104ea19aeb9b5e1c1bce1e0271d80974
ronpandolfi <ronpandolfi@gmail.com>
2021-03-17 16:26:30 -0700

    Add category to diffusion coefficient op

--- xicam/SAXS/operations/diffusion_coefficient.py
+++ xicam/SAXS/operations/diffusion_coefficient.py
@@ -1,9 +1,10 @@
 import numpy as np
-from xicam.plugins.operationplugin import operation, output_names, display_name
+from xicam.plugins.operationplugin import operation, output_names, display_name, categories
 
 
 @operation
 @output_names('diffusion_coefficient')
 @display_name('Diffusion Coefficient')
+@categories(('XPCS',))
 def diffusion_coefficient(roi_q: np.ndarray, relaxation_rates: np.ndarray):
     return np.average(relaxation_rates / roi_q ** 2)
