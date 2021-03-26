adb191bc69d2af04a803b6b203da3f87128703b2
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-03-26 11:25:34 -0700

    Update 2time op sliceselector to use indexes corresponding to avg qs of the labels

--- xicam/SAXS/operations/twotime.py
+++ xicam/SAXS/operations/twotime.py
@@ -1,5 +1,4 @@
 import pyqtgraph as pg
-from xicam.SAXS.utils import get_label_array
 from xicam.core import msg
 from xicam.core.intents import ImageIntent
 from xicam.plugins.operationplugin import operation, describe_input, describe_output, visible, \
@@ -7,6 +6,9 @@ from xicam.plugins.operationplugin import operation, describe_input, describe_ou
 import numpy as np
 from skbeam.core.correlation import two_time_corr
 from typing import Tuple, Iterable
+
+from ..patches.pyFAI import AzimuthalIntegrator
+from ..utils import average_q_from_labels, get_label_array
 
 
 @operation
@@ -18,21 +20,22 @@ from typing import Tuple, Iterable
 @describe_input('num_bufs', 'maximum lag step to compute in each generation of downsampling (must be even)')
 @describe_input('num_levels', 'how many generations of downsampling to perform,'
                               'i.e., the depth of the binomial tree of averaged frames default is one')
-@output_names('g2', 'tau')
+@output_names('g2', 'tau', 'qs')
 @describe_output('g2', 'the normalized correlation shape is (num_rois, len(lag_steps), len(lag_steps))')
 @describe_output('tau', 'the times at which the correlation was computed')
 @visible('images', False)
 @visible('rois', False)
 @intent(ImageIntent,
         name='2-time Correlation',
-        output_map={'image': 'g2'},
+        output_map={'image': 'g2', 'xvals': 'qs'},
         mixins=["AxesLabels", "XArrayView", "SliceSelector"],
         labels={"bottom": "𝜏₁", "left": "𝜏₂"})
 def two_time_correlation(images: np.ndarray,
                          image_item: pg.ImageItem = None,
                          rois: Iterable[pg.ROI] = None,
                          num_bufs: int = 16,
-                         num_levels: int = 8) -> Tuple[np.ndarray, np.ndarray]:
+                         num_levels: int = 8,
+                         geometry: AzimuthalIntegrator = None) -> Tuple[np.ndarray, np.ndarray]:
     # TODO -- make composite parameter item widget to allow default (all frames) or enter value
     num_frames = len(images)
 
@@ -48,7 +51,16 @@ def two_time_correlation(images: np.ndarray,
                          num_levels)
     g2 = corr.g2
     lag_steps = corr.lag_steps
-    return np.rot90(g2, axes=(-2, -1)), lag_steps
+
+    # Calculate avg qs from label array for first dimension on returned g2 (so slice selector shows qs for indexing)
+    qs = None
+    if geometry is not None:
+        qs = np.asarray(average_q_from_labels(labels, geometry))
+
+    # Rotate image plane 90 degrees
+    g2 = np.rot90(g2, axes=(-2, -1))
+
+    return g2, lag_steps, qs
 
 
 # class TwoTimeCorrelation(ProcessingPlugin):
