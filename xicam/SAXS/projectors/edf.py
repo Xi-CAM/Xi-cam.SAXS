a211a87108e46ec960ea5639784f6b158f88850c
Ronald Pandolfi <ronpandolfi@gmail.com>
2021-04-01 16:34:45 -0700

    Reduce angle var to scalar; more permissive projection

--- xicam/SAXS/projectors/edf.py
+++ xicam/SAXS/projectors/edf.py
@@ -70,6 +70,7 @@ def project_NXsas(run_catalog):
         poni1 = poni1[0].values.max()
         rot2 = detector_rotation[0].values.max()
         wavelength = wavelength[0].values.max()
+        incidence_angle = incidence_angle[0].values.max()
 
         # Create detector from projection metadata
         detector_name = projection['configuration']['detector_name']
@@ -91,7 +92,7 @@ def project_NXsas(run_catalog):
         msg.logMessage(e, level=msg.WARNING)
 
     intents_list = []
-    if projection['configuration']['geometry_mode'] == 'reflection':
+    if projection['configuration'].get('geometry_mode') == 'reflection':
         intents_list.append(GISAXSImageIntent(image=data,
                                               name='Scattering Image Data',
                                               geometry=geometry,
