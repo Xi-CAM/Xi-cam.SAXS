cd78c8b9941e809aaad65bf384bbc87ddd333113
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-05-10 11:06:45 -0700

    make correction code more reachable by other modules

--- xicam/SAXS/operations/correction.py
+++ xicam/SAXS/operations/correction.py
@@ -4,6 +4,41 @@ from xicam.plugins.operationplugin import operation, display_name, output_names,
     describe_output, categories, visible
 
 
+@njit(parallel=True)
+def correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
+    # 16-bit unsigned
+    # image: bits 0 - 12
+    # bad:   bit 13
+    # gain:  bits 14-15
+    # gain1: 0b11 (3)
+    # gain2: 0b10 (2)
+    # gain8: 0b00 (0)
+    array = array.astype(dtype=np.uint16)
+    flats = flats.astype(dtype=np.float32)
+    bkg = bkg.astype(dtype=np.float32)
+
+    for i in prange(array.shape[0]):
+        for j in prange(array.shape[1]):
+            for k in prange(array.shape[2]):
+                val = array[i, j, k]
+
+                intensity = 0x1FFF & val
+                bad_flag = 0x1 & (val >> 13)
+                gain = gain_map[0x3 & (val >> 14)]
+
+                array[i, j, k] = flats[j, k] * gain * intensity
+
+                if val < bkg[j, k]:
+                    array[i, j, k] = bkg[j, k]
+
+                if bad_flag:
+                    array[i, j, k] = 0
+                else:
+                    array[i, j, k] -= bkg[j, k]
+
+    return array
+
+
 @operation
 @display_name('FastCCD Correction')
 @describe_input('images', '3-dimensional input array containing 2-dimensional images')
@@ -20,37 +55,6 @@ def correct_fastccd_image(images: np.ndarray,
                           flats: np.ndarray = None,
                           darks: np.ndarray = None,
                           gains: tuple = (1, 2, 4, 8)) -> np.ndarray:
-
-    @njit(parallel=True)
-    def correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
-        # 16-bit unsigned
-        # image: bits 0 - 12
-        # bad:   bit 13
-        # gain:  bits 14-15
-        # gain1: 0b11 (3)
-        # gain2: 0b10 (2)
-        # gain8: 0b00 (0)
-
-        for i in prange(array.shape[0]):
-            for j in prange(array.shape[1]):
-                for k in prange(array.shape[2]):
-                    val = array[i,j,k]
-
-                    intensity = 0x1FFF & val
-                    bad_flag = 0x1 & (val >> 13)
-                    gain = gain_map[0x3 & (val >> 14)]
-
-                    array[i,j,k] = flats[j,k] * gain * intensity
-
-                    if val < bkg[j,k]:
-                        array[i,j,k] = bkg[j,k]
-
-                    if bad_flag:
-                        array[i,j,k] = 0
-                    else:
-                        array[i,j,k] -= bkg[j,k]
-
-        return array
 
     # TODO: is this pulling from the correct dark? we need of mapping of gain index to dark array?
 
@@ -71,9 +75,9 @@ def correct_fastccd_image(images: np.ndarray,
     else:
         darks = np.sum(darks, axis=0) / darks.shape[0]
 
-    corrected_images = correct(np.asarray(images, dtype=np.uint16),
-                               np.asarray(flats, dtype=np.float32),
-                               np.asarray(darks, dtype=np.float32),
+    corrected_images = correct(images,
+                               flats,
+                               darks,
                                gains)
     return corrected_images
 
