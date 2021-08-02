2da0440945bba5fe24ebd6fccf2d0a7683cf0ab2
Ian Humphrey <ian.r.humphrey@gmail.com>
2021-08-02 16:09:08 -0700

    fix for single frame images; fix for updating current frame on multi-frame images

--- xicam/SAXS/widgets/imageviewmixins.py
+++ xicam/SAXS/widgets/imageviewmixins.py
@@ -2,7 +2,7 @@ from databroker import Broker
 from qtpy.QtWidgets import QPushButton, QSizePolicy
 import numpy as np
 
-from xicam.gui.widgets.imageviewmixins import BetterLayout, ProcessingView, XArrayView
+from xicam.gui.widgets.imageviewmixins import BetterLayout, ProcessingView
 
 from xicam.SAXS.operations.correction import correct
 
@@ -10,29 +10,29 @@ from xicam.SAXS.operations.correction import correct
 class BackgroundCorrected(BetterLayout, ProcessingView):
     """Toggle background correction for an image or image series."""
     def __init__(self, *args, darks=None, **kwargs):
-        super(BackgroundCorrected, self).__init__(*args, **kwargs)
         self._darks = None
+        self.set_darks(darks)
         self._bg_correction = False
         self._bg_correct_btn = QPushButton("BG Correction")
         self._bg_correct_btn.setEnabled(False)
         self._bg_correct_btn.setCheckable(True)
         self._bg_correct_btn.clicked.connect(self._toggle_bg_correction)
+
+        super(BackgroundCorrected, self).__init__(*args, **kwargs)
+
         size_policy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         size_policy.setHorizontalStretch(0)
         size_policy.setVerticalStretch(1)
         size_policy.setHeightForWidth(self._bg_correct_btn.sizePolicy().hasHeightForWidth())
         self._bg_correct_btn.setSizePolicy(size_policy)
-
-        self.set_darks(darks)
-
         self.ui.right_layout.addWidget(self._bg_correct_btn)
 
     def setImage(self, img, *args, darks=None, **kwargs):
         """Override to add additional `darks` kwarg for dark image."""
-        super(BackgroundCorrected, self).setImage(img, *args, **kwargs)
         if darks is not None and self._darks is not None:
             self._darks = darks
         self.set_darks(darks)  # Can't do this before... self.image won't be set until super call
+        super(BackgroundCorrected, self).setImage(img, *args, **kwargs)
 
     def set_darks(self, darks):
         """Set dark image for this image view."""
@@ -45,7 +45,14 @@ class BackgroundCorrected(BetterLayout, ProcessingView):
         """Slot to handle when bg correction button is clicked."""
         self._bg_correction = value
         self._bg_correct_btn.setChecked(value)
-        self.getProcessedImage()  # TODO: how do we make this work for a single frame image / update current frame?
+        index = self.currentIndex
+        self.setImage(self.image, autoRange=False)  # Reprocess the current frame
+        if self.image is not None:
+            try:
+                self.setCurrentIndex(index)
+            except TypeError as e:
+                # Ignore "tuple indices must be integers or slices, not NoneType" for 2d pseudo3framearray images
+                pass
 
     def process(self, image):
         """Either returns the raw image or the correct image, depending on the bg correction button state."""
