299f0020ea1fc8eae9bc625d6c11135fc3de571a
ronpandolfi <ronpandolfi@gmail.com>
2021-09-08 11:47:53 -0700

    Fix clipping below zero

--- xicam/SAXS/operations/correction.py
+++ xicam/SAXS/operations/correction.py
@@ -4,13 +4,14 @@ from xicam.plugins.operationplugin import operation, display_name, output_names,
     describe_output, categories, visible
 
 
-def correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
+def correct(array, flats, bkg, gain_map=(1, 2, 4, 8), clip=False):
     return _correct(np.asarray(array, dtype=np.uint16),
                     np.asarray(flats, dtype=np.uint16),
-                    np.asarray(bkg, dtype=np.uint16))
+                    np.asarray(bkg, dtype=np.uint16),
+                    clip=clip)
 
 @njit(parallel=True)
-def _correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
+def _correct(array, flats, bkg, gain_map=(1, 2, 4, 8), clip=False):
     # 16-bit unsigned
     # image: bits 0 - 12
     # bad:   bit 13
@@ -33,15 +34,16 @@ def _correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
 
                 array[i, j, k] = masked_flat[j, k] * gain * intensity
 
-                if intensity < masked_dark[j, k]:
-                    array[i, j, k] = bkg_gain * masked_dark[j, k]
+                if clip:
+                    if intensity < masked_dark[j, k]:
+                        array[i, j, k] = bkg_gain * masked_dark[j, k]
 
                 if bad_flag:
                     array[i, j, k] = 0
                 else:
                     array[i, j, k] -= bkg_gain * masked_dark[j, k]
 
-    return array
+    return array.astype(np.int16)
 
 
 @operation
@@ -59,7 +61,8 @@ def _correct(array, flats, bkg, gain_map=(1, 2, 4, 8)):
 def correct_fastccd_image(images: np.ndarray,
                           flats: np.ndarray = None,
                           darks: np.ndarray = None,
-                          gains: tuple = (1, 2, 4, 8)) -> np.ndarray:
+                          gains: tuple = (1, 2, 4, 8),
+                          clip_below_dark: bool = False) -> np.ndarray:
 
     # TODO: is this pulling from the correct dark? we need of mapping of gain index to dark array?
 
@@ -75,14 +78,15 @@ def correct_fastccd_image(images: np.ndarray,
     darks = darks
     if darks is None:
         darks = np.zeros_like(images[0])
-    elif darks.ndim != 3:
-        raise ValueError(f"\"darks\" should be 3-dimensional; shape = \"{darks.shape}\"")
-    else:
+    elif darks.ndim == 3:
         darks = np.sum(darks, axis=0) / darks.shape[0]
+    elif darks.ndim == 2:
+        pass  # darks is already a single frame
 
     corrected_images = correct(images,
                                flats,
                                darks,
-                               gains)
+                               gains,
+                               clip=clip_below_dark)
     return corrected_images
 
