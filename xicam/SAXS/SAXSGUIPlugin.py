91d17af2a1d034d3837e31eb7bebd52b02a6e549
ronpandolfi <ronpandolfi@gmail.com>
2022-06-16 18:17:52 -0700

    reorder plugin stages

--- xicam/SAXS/SAXSGUIPlugin.py
+++ xicam/SAXS/SAXSGUIPlugin.py
@@ -4,5 +4,5 @@ from xicam.core.data import ProjectionException
 from xicam.SAXS.stages import CorrelationStage, CalibrateGUIPlugin, ReduceGUIPlugin
 
 
-class SAXSPlugin(CalibrateGUIPlugin, ReduceGUIPlugin, CorrelationStage):  # ...):
+class SAXSPlugin(CorrelationStage, ReduceGUIPlugin, CalibrateGUIPlugin):  # ...):
     name = 'SAXS'
